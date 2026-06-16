f9d20e4b3c2b8586506c77d070fbbd5b35aaf749
Vedant Varma <vedantvarma617@gmail.com>
2026-06-16 19:46:33 +0530

    Created User-model and updated the auth-controller.js file -> changes made in the auth-controller files are : Add user creation logic with duplicate email validation

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("Welcome to Vedant World from Router  ! ");
@@ -8,11 +10,28 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body); 
-    res.status(200).json({message : req.body }); 
+    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists !" });
+    }
+
+    const userCreated = await User.create({
+      username,
+      email,
+      phone,
+      password,
+    });
+
+    console.log(userCreated);
+
+    res.status(200).json({ message: req.body });
   } catch (error) {
     res.status(400).json("Page not found ");
   }
 };
 
-module.exports = { home , register } ; 
+module.exports = { home, register };
--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const connectDb = async () => {
-    try {
-        if (!process.env.MONGODB_URI) {
-            console.error("Error: MONGODB_URI is not defined in your .env file!");
-            process.exit(1);
-        }
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connection successful to DB");
-    } catch (error) {
-        console.error("Database connection failed details:", error.message); // This will tell us the exact issue
-        process.exit(1);
+const URI = process.env.MONGODB_URI;
+
+
+const connectDb = async () => { 
+    try { 
+        await mongoose.connect(URI); 
+        console.log("Connection sucessfull to DB ");
+
+    } catch (error) { 
+        console.error("database conncetion failed ! "); 
+        process.exit(0); 
     }
 };
 
