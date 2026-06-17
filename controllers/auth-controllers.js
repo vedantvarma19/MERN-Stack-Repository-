186871c8df87fcf17a06137c7255f41785cce297
Vedant Varma <vedantvarma617@gmail.com>
2026-06-17 18:17:00 +0530

    implemented Bcryptjs for password hashing

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 
 const home = async (req, res) => {
   try {
@@ -19,16 +20,21 @@ const register = async (req, res) => {
       return res.status(400).json({ msg: "email already exists !" });
     }
 
+    // hash password 
+
+    const saltRound = 10 ; 
+    const hash_Password = await bcrypt.hash(password , saltRound);
+
     const userCreated = await User.create({
       username,
       email,
       phone,
-      password,
+      password : hash_Password,
     });
 
     console.log(userCreated);
 
-    res.status(200).json({ message: req.body });
+    res.status(200).json({ message: "User Created !" });
   } catch (error) {
     res.status(400).json("Page not found ");
   }
